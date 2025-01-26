225c843345d2083baab6a4d9e1f5cd15b2bea418
WorkAccVikas <workaccvikas@gmail.com>
2025-01-26 22:40:24 +0530

    prettier setup with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,32 @@
-import pluginJs from "@eslint/js";
+import pluginJs from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ["node_modules", "dist"],
+    ignores: ['node_modules', 'dist'],
   },
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
+
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
-        process: "readonly", // Define process as a global variable
-        console: "readonly", // Allow use of console
-        structuredClone: "readonly", // Allow use of structuredClone
+        process: 'readonly', // Define process as a global variable
+        console: 'readonly', // Allow use of console
+        structuredClone: 'readonly', // Allow use of structuredClone
       },
     },
+
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
     rules: {
+      ...pluginJs.configs.recommended.rules, // Include recommended rules from `@eslint/js`
+      'prettier/prettier': 'error', // Enable Prettier rules
       // 'no-console': 'off', // Disable the rule to allow console.log
-      "no-console": "error",
+      'no-console': 'error',
       // quotes: ["error", "single", { allowTemplateLiterals: true }],
     },
   },
